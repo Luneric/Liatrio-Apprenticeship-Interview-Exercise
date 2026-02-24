13cdf14b7be94d5a6f793a2b671218be903b64fc
Carolyn Thai <thaicarolyn8k@gmail.com>
2026-02-24 03:52:19 -0800

    Fixing output to minified JSON

--- webapp/main.go
+++ webapp/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"time"
 
-	"encoding/json"
-
 	"github.com/gofiber/fiber/v3"
 )
 
@@ -12,19 +10,10 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
-		payload := fiber.Map{
+		return c.JSON(fiber.Map{
 			"message":   "My name is Carolyn Thai",
 			"timestamp": time.Now().UnixMilli(),
-		}
-
-		c.Set("Content-Type", "application/json")
-
-		err := json.NewEncoder(c.Response().BodyWriter()).Encode(payload)
-		if err != nil {
-			return c.Status(500).SendString("Encoder Error")
-		}
-
-		return nil
+		})
 	})
 
 	app.Listen(":3000")
